c9dc3fc436987c3338ea45b611ed50ab23b22940
Grigoriy Lyalko <98054846+Greg2089@users.noreply.github.com>
2024-02-08 17:37:59 +0300

    4) remove Note

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -53,6 +53,13 @@ public class MainActivity extends AppCompatActivity {
                     linearLayoutNotes,
                     false
             );
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    database.remove(note.getId());
+                    showNotes();
+                }
+            });
 
             TextView textViewNote = view.findViewById(R.id.textViewNote);
             textViewNote.setText(note.getText());
