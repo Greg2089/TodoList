c0cb2c5ebfdd0c0518eedf470c0f95dc734b1d2b
Grigoriy Lyalko <98054846+Greg2089@users.noreply.github.com>
2024-02-08 13:17:22 +0300

    1) Layout

---
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.todolist;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private LinearLayout linearLayoutNotes;
+    private FloatingActionButton buttonAddNote;
+
+    private final ArrayList<Note> notes = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        Random random = new Random();
+        for (int i = 0; i < 20; i++) {
+            Note note = new Note(i, "Note " + i, random.nextInt(3));
+            notes.add(note);
+        }
+        showNotes();
+    }
+
+    private void initViews() {
+        linearLayoutNotes = findViewById(R.id.linearLayoutNotes);
+        buttonAddNote = findViewById(R.id.buttonAddNote);
+    }
+
+    private void showNotes() {
+        for (Note note : notes) {
+            View view = getLayoutInflater().inflate(
+                    R.layout.note_item,
+                    linearLayoutNotes,
+                    false
+            );
+            TextView textViewNote = view.findViewById(R.id.textViewNote);
+            textViewNote.setText(note.getText());
+
+            int colorResId;
+            switch (note.getPriority()) {
+                case 0:
+                    colorResId = android.R.color.holo_green_light;
+                    break;
+                case 1:
+                    colorResId = android.R.color.holo_orange_light;
+                    break;
+                default:
+                    colorResId = android.R.color.holo_red_light;
+                    break;
+            }
+
+            int color = ContextCompat.getColor(this, colorResId);
+            textViewNote.setBackgroundColor(color);
+            linearLayoutNotes.addView(view);
+        }
+    }
+}
